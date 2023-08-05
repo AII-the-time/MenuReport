9b3dae8a7db8851e98dcd2680a0c260a10488bd2
raipen <raipendalk@gmail.com>
2023-08-06 07:01:03 +0900

    feat: update input page header design

---
+++ front/src/page/Input/components.js
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import {ButtonCSS,InputCSS} from '../../components';
+
+export const LinkButton = styled.button`
+    ${ButtonCSS}
+    margin: 30px 0 0 auto;
+`;
+
+export const PlusButton = styled.button`
+    ${ButtonCSS}
+}`;
+
+export const TwoInputWithLabel = styled.div`
+  display: flex;
+  gap: 10px;
+  width: 100%;
+  > div {
+    width: 1%;
+    flex: 1 1 0;
+    display: flex;
+    flex-direction: column;
+    > label {
+      font-size: 1.15rem;
+      font-weight: bold;
+    }
+    > input {
+      ${InputCSS}
+      margin: 0;
+    }
+    >div{
+      position: relative;
+      display: flex;
+      > input {
+          ${InputCSS}
+          width: 100%;
+          margin: 0;
+      }
+      > span {
+          display: block;
+          position: absolute;
+          right: 10px;
+          top: 50%;
+          transform: translateY(-50%);
+      }
+    }
+  }
+  margin: 6px 0 16px 0;
+`;
+
+export const LabelWithTooltip = styled.label`
+    margin: 10px 0 6px 0;
+    font-size: 1.15rem;
+    font-weight: bold;
+    display: flex;
+    align-items: baseline;
+    position: relative;
+    > .material-icons {
+        font-size: 1.15rem;
+        > div {
+            font-family: 'Noto Sans KR', sans-serif;
+            display: none;
+            position: absolute;
+            top: 100%;
+            left: 0;
+            width: 100%;
+            white-space: wrap;
+            word-break: keep-all;
+            background-color: #fff;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            padding: 10px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+            z-index: 1;
+            line-height: normal;
+            grid-template-columns: auto 1fr;
+            grid-gap: 10px;
+            font-size: 1rem;
+        }
+    }
+    > .material-icons:hover {
+        > div {
+            display: grid;
+        }
+    }
+`;
--- front/src/page/Input/index.js
+++ front/src/page/Input/index.js
@@ -1,20 +1,9 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { useState, useRef, useEffect } from 'react';
+import {useSearchParams, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { ContentsWrapper } from '../../components';
+import { LinkButton, PlusButton,TwoInputWithLabel, LabelWithTooltip } from './components';
 
-const MenuRecipe = styled.div``;
-const MenuInfo = styled.div`
-  display: flex;
-`;
-const MenuName = styled.span`
-  align-items: center;
-  padding: 5px;
-  display: flex;
-`;
-const MenuPrice = styled.span`
-  align-items: center;
-  padding: 10px;
-  display: flex;
-`;
 const PreInput = styled.div`
   display: flex;
   margin: 10px;
@@ -35,96 +24,105 @@ const RecipeInput = styled.div``;
 const Btn = styled.button``;
 
 export default function App() {
+  const [nextLoding, setNextLoding] = useState(false);
+
+  const onClick = () => {
+    setNextLoding(true);
+  };
   return (
-    <div className="App">
-      <MenuRecipe>
-        <MenuInfo>
-          <MenuName>
-            메뉴 이름
-            <Input />
-          </MenuName>
-          <MenuPrice>
-            가격
-            <Input />
-          </MenuPrice>
-        </MenuInfo>
-        <PreInput>
-          원두
-          <ChooseOption>
-            <Option>
-              <OptionList>
-                <tr>
-                  <Input type="checkbox" />
-                  에티오피아
-                </tr>
-                <tr>
-                  <Input type="checkbox" />
-                  콜롬비아
-                </tr>
-                <tr>
-                  <Input type="checkbox" />
-                  사용안함
-                </tr>
-              </OptionList>
-            </Option>
-            <Amount>
-              <Input />g
-            </Amount>
-          </ChooseOption>
-        </PreInput>
-        <PreInput>
-          우유
-          <ChooseOption>
-            <Option>
-              <OptionList>
-                <tr>
-                  <Input type="checkbox" />
-                  에티오피아
-                </tr>
-                <tr>
-                  <Input type="checkbox" />
-                  콜롬비아
-                </tr>
-                <tr>
-                  <Input type="checkbox" />
-                  사용안함
-                </tr>
-              </OptionList>
-            </Option>
-            <Amount>
-              <Input />g
-            </Amount>
-          </ChooseOption>
-        </PreInput>
-        <PreInput>
-          포장재
-          <ChooseOption>
-            <Option>
-              <OptionList>
-                <td>
-                  <Input type="checkbox" />
-                  음료용
-                </td>
-                <td>
-                  <Input type="checkbox" />
-                  디저트 박스
-                </td>
-                <td>
-                  <Input type="checkbox" />
-                  사용안함
-                </td>
-              </OptionList>
-            </Option>
-          </ChooseOption>
-        </PreInput>
-        <RecipeInput>
-          <Input />
+    <ContentsWrapper>
+      <TwoInputWithLabel>
+        <div>
+          <label>메뉴 이름</label>
+          <input name="menuName" />
+        </div>
+        <div>
+          <label>메뉴 가격</label>
+          <div>
+            <input name="menuPrice" type="number" />
+            <span>원</span>
+          </div>
+        </div>
+      </TwoInputWithLabel>
+      <PreInput>
+        원두
+        <ChooseOption>
+          <Option>
+            <OptionList>
+              <tr>
+                <Input type="checkbox" />
+                에티오피아
+              </tr>
+              <tr>
+                <Input type="checkbox" />
+                콜롬비아
+              </tr>
+              <tr>
+                <Input type="checkbox" />
+                사용안함
+              </tr>
+            </OptionList>
+          </Option>
           <Amount>
             <Input />g
           </Amount>
-        </RecipeInput>
-        <Btn>추가</Btn>
-      </MenuRecipe>
-    </div>
+        </ChooseOption>
+      </PreInput>
+      <PreInput>
+        우유
+        <ChooseOption>
+          <Option>
+            <OptionList>
+              <tr>
+                <Input type="checkbox" />
+                에티오피아
+              </tr>
+              <tr>
+                <Input type="checkbox" />
+                콜롬비아
+              </tr>
+              <tr>
+                <Input type="checkbox" />
+                사용안함
+              </tr>
+            </OptionList>
+          </Option>
+          <Amount>
+            <Input />g
+          </Amount>
+        </ChooseOption>
+      </PreInput>
+      <PreInput>
+        포장재
+        <ChooseOption>
+          <Option>
+            <OptionList>
+              <td>
+                <Input type="checkbox" />
+                음료용
+              </td>
+              <td>
+                <Input type="checkbox" />
+                디저트 박스
+              </td>
+              <td>
+                <Input type="checkbox" />
+                사용안함
+              </td>
+            </OptionList>
+          </Option>
+        </ChooseOption>
+      </PreInput>
+      <RecipeInput>
+        <Input />
+        <Amount>
+          <Input />g
+        </Amount>
+      </RecipeInput>
+      <PlusButton>+ 메뉴 추가</PlusButton>
+      <LinkButton onClick={nextLoding ? (e)=>{e.preventDefault()} : onClick} type="submit">
+        {nextLoding ? '메뉴 리포트 생성 중...' : '다음으로'}
+      </LinkButton>
+    </ContentsWrapper>
   );
 }
