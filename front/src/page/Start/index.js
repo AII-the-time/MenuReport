27c998224fb009aed8ff3cc09c83c320b9573143
raipen <raipendalk@gmail.com>
2023-08-05 20:29:03 +0900

    feat:set main page

--- front/src/page/Start/index.js
+++ front/src/page/Start/index.js
@@ -1,8 +1,50 @@
-import {Link} from 'react-router-dom';
+import {Link, useSearchParams} from 'react-router-dom';
+import styeled from 'styled-components';
+
+const ContentsWrapper = styeled.div`
+    background-color: #fff;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+    align-items: start;
+    padding: 30px 10px 0 10px;
+    max-width: 600px;
+`;
+
+const LinkButton = styeled(Link)`
+    background-color: #fff;
+    margin: 5px auto;
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+    align-items: center;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 10px 30px;
+    position: relative;
+    cursor: pointer;
+    &:hover{
+        background-color: var(--main-color);
+        color: #fff;
+        text-decoration: none;
+    }
+`;
+
 export default function(){
-    return (
-        <Link to="/PreInput">
-            시작하기
-        </Link>
+    const [searchParams] = useSearchParams();
+    return(
+        <ContentsWrapper>
+            <h1>안녕하세요</h1>
+            <p>저희는 대한민국 과학기술정보통신부에서 주관하는 SW마에스트로 ~~</p>
+            <br/>
+            <LinkButton to={`/PreInput?userId=${searchParams.get('userId')}`}>
+                메뉴 레포트 체험하기
+            </LinkButton>
+            <div>
+                * 유의사항: ~~~~~~~~~~~~
+            </div>
+        </ContentsWrapper>
     );
 }
